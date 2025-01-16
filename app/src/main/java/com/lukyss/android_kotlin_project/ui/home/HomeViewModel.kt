7afc4c2e4484873f0dadcb58da547f1d6bf7994f
Lukyss (Quentin Lachery) <quentin.wow@free.fr>
2025-01-16 12:37:28 +0100

    first commit

---
+++ app/src/main/java/com/lukyss/android_kotlin_project/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.lukyss.android_kotlin_project.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
